f5fa79f6c0ce5a7983dccc4ea0605b9628f5a27b
ChenFrancis <cb331339299@163.com>
2014-06-17 16:18:02 +0800

    创建Foundation和UIKit例子练习项目

---
+++ CBExampleAPI/CBExampleAPI/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  CBExampleAPI
+//
+//  Created by xychen on 14-6-17.
+//  Copyright (c) 2014年 CB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"加载了%@", NSStringFromClass([ViewController class]));
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
