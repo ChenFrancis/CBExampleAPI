fca91771b61360c35c19735031a6295bcba831bf
ChenFrancis <cb331339299@163.com>
2014-06-27 20:18:03 +0800

    实例化日历

--- CBExampleAPI/CBExampleAPI/Calendar/CalendarDemoViewController.m
+++ CBExampleAPI/CBExampleAPI/Calendar/CalendarDemoViewController.m
@@ -27,6 +27,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
 }
 
 - (void)didReceiveMemoryWarning
